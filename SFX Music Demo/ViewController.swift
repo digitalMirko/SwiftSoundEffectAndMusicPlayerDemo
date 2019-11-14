0ed1fb451d0a5e26ac5533cfc72d74c00f743e38
Mirko Cukich <mirkocukich@gmail.com>
2019-11-13 21:35:24 -0600

    IBOutlets and IBActions added to ViewController_swift

--- SFX Music Demo/ViewController.swift
+++ SFX Music Demo/ViewController.swift
@@ -8,14 +8,56 @@
 // Swift Sound Effect and Music Player Demo - Demo 28 of 30
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    
+    // Slider controls
+    @IBOutlet weak var soundVolume: UISlider!
+    @IBOutlet weak var musicVolume: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-
+    // Sound file Button / Slider control
+    @IBAction func playSoundBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func soundVolumeSlider(_ sender: Any) {
+        
+    }
+    
+    
+    // Music file Buttons / Slider control
+    @IBAction func playMusicBtn(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func pauseMusicBtn(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func restartMusicBtn(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func stopMusicBtn(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func musicVolumeSlider(_ sender: Any) {
+        
+    }
+    
+    
 }
 
